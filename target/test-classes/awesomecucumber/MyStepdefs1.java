cf2fdcdba3e44a7507b7cc40f46762081537dfdb
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-12-28 10:16:58 +0530

    commit

---
+++ target/test-classes/awesomecucumber/MyStepdefs1.java
@@ -0,0 +1,10 @@
+package awesomecucumber;
+
+import io.cucumber.java.en.When;
+
+public class MyStepdefs1 {
+    @When("I do dummy things{int}")
+    public void iDoDummyThings(int arg0) {
+
+    }
+}
